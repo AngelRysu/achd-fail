e71721ec7fcdd16b9631bc150d49132706b50ab8
Alejandro Amavizca <alejandroaamavizcal@gmail.com>
2024-01-24 22:32:54 +0000

    ya jalan los archivos css img y js

--- mysite/app.py
+++ mysite/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 from flask_orator import Orator
 from configparser import ConfigParser
 config = ConfigParser()
@@ -27,8 +27,20 @@ DATABASES = {
 app.config['ORATOR_DATABASES'] = DATABASES
 db = Orator(app)
 
+@app.route('/img/<filename>', methods=['GET'])
+def route_img_files(filename):
+    return send_from_directory('templates/img', path=filename)
+
+
+@app.route('/css/<filename>')
+def route_js_files(filename):
+    return send_from_directory('templates/css', path=filename)
+
+@app.route('/js/<filename>')
+def route_css_files(filename):
+    return send_from_directory('templates/js', path=filename)
+
 @app.route('/')
 def hello_world():
-    retrieve = db.table ('datos_ejemplo').get().serialize()
-    return str(retrieve)
+    return render_template('index.html')
 
